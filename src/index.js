01771d27c44f807dfa7644c79c534eca5b41f1c8
mediasnunes <codewithmadu@gmail.com>
2024-09-08 22:12:42 -0300

    fix load page

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ let humidity = document.querySelector("#humidity");
 let windSpeed = document.querySelector("#wind-speed");
 let description = document.querySelector("#description");
 let iconElement = document.querySelector("#icon");
-console.log(iconElement)
 let form = document.querySelector("#city-form");
 let newCity = document.querySelector("#city-input");
 let apiKey = "3ab87d04f7c37deb36b8t663978f41oc";
@@ -26,10 +25,13 @@ function changeTemperature(response) {
 function changeCity(event) {
   event.preventDefault();
   let cityName = newCity.value;
+searchCity(cityName);
+}
 
-  //Call API
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cityName}&key=${apiKey}`;
-  axios.get(apiUrl).then(changeTemperature);
+function searchCity(city){
+    //Call API
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    axios.get(apiUrl).then(changeTemperature);
 }
 
 form.addEventListener("submit", changeCity);
@@ -53,3 +55,4 @@ let date = document.querySelector("#time");
 let formatedDate = `${weekDay} ${hour}:${minutes}`;
 
 date.innerHTML = `${formatedDate}`;
+searchCity("Paris");
